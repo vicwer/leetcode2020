fdede825251fd787b3647ce25f838935f1afaf81
KG Duan <876706485@qq.com>
2020-05-08 21:37:40 +0800

    二叉树的最大路径和 hardhardhard

---
+++ 124.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+struct BinaryTree
+{
+    int val;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+
+int result = 0;
+
+int dfs(BinaryTree* root)
+{
+    if(!root)
+        return 0;
+
+    int left = max(dfs(root->left), 0);
+    int right = max(dfs(root->right), 0);
+    result = max(root->val + left + right, right);
+    return max(0, max(left, right))+root->val;
+}
+
+int maxPathSum(BinaryTree* root)
+{
+    if(!root)
+        return 0;
+    result = root->val;
+    dfs(root);
+    return result;
+}
