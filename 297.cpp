57b8714625fc5002483b6151a8e79655e5283da5
Kaige Duan <vicwer@users.noreply.github.com>
2020-05-13 12:40:09 +0800

    二叉树的序列化与反序列化 莫名其妙的感觉阿。。

--- 297.cpp
+++ 297.cpp
@@ -2,58 +2,63 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <sstream>
 
-struct BinaryTree
+struct TreeNode
 {
     int val;
-    BinaryTree* left;
-    BinaryTree* right;
+    TreeNode* left;
+    TreeNode* right;
 
-    BinaryTree(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-int depth(BinaryTree* root)
+int maxDepth(TreeNode* root)
 {
     if(!root)
         return 0;
-
-    return std::max(depth(root->left), depth(root->right)) + 1;
+    else
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
 }
 
-std::string serialize(BinaryTree* root)
+std::string serialize(TreeNode* root)
 {
-    std::string res;
-
+    std::string res = "";
     if(!root)
         return res;
 
-    int depth = depth(root);
+    int depth = maxDepth(root);
 
-    std::queue<BinaryTree*> cnt_layer;
+    std::queue<TreeNode*> cnt_layer;
     cnt_layer.push(root);
 
-    int cnt_depth = 1;
+    int cnt_depth = 0;
     while(!cnt_layer.empty())
     {
         cnt_depth += 1;
+
         int size = cnt_layer.size();
-        for(int i = 0; i < size; i++)
+        for(int i = 0 ;i < size; i++)
         {
-            BinaryTree* cnt_root = cnt_layer.front();
+            TreeNode* cnt_root = cnt_layer.front();
             cnt_layer.pop();
-            res = res + std::to_string(cnt_root->val) + ",";
-            if(cnt_root->left)
-                cnt_layer.push(cnt_root->left);
-            else if(cnt_depth < depth)
-                res = res + "null" + ",";
-            if(cnt_root->right)
-                cnt_layer.push(cnt_root->right);
-            else if(cnt_depth < depth)
-                res = res + "null" + ",";
+            if(!cnt_root)
+            {
+                res = res + "#,";
+                continue;
+            }
+            else
+            {
+                res = res + std::to_string(cnt_root->val)+",";
+                if(cnt_depth < depth)
+                {
+                    cnt_layer.push(cnt_root->left);
+                    cnt_layer.push(cnt_root->right);
+                }
+            }
         }
     }
     res.pop_back();
-
     res = "[" + res + "]";
 
     return res;
@@ -61,12 +66,12 @@ std::string serialize(BinaryTree* root)
 
 std::vector<std::string> split(std::string data)
 {
-    data.pop_back();
     data.erase(data.begin());
+    data.pop_back();
     std::vector<std::string> res;
-    std::istringstream iss(data);
-    std::string tmp;
+    istringstream iss(data);
 
+    std::string tmp;
     while(getline(iss, tmp, ','))
     {
         res.push_back(tmp);
@@ -74,45 +79,38 @@ std::vector<std::string> split(std::string data)
 
     return res;
 }
-std::string deserialize(std::string data)
+
+TreeNode* deserialize(std::string data)
 {
-    
     if(data.empty())
         return NULL;
 
     std::vector<std::string> data_list = split(data);
 
-    if(data_list[0] == "null")
-        return NULL;
-
-    BinaryTree* root = new BinaryTree(atoi(data_list[0].c_str()));
-
-    std::queue<BinaryTree*> cnt_layer;
+    TreeNode* root = new TreeNode(atoi(data_list[0].c_str()));
+    std::queue<TreeNode*> cnt_layer;
     cnt_layer.push(root);
-    for(int i = 1; i < data_list.size(); i++)
+
+    for(int i = 1; i < data_list.size();)
     {
-        BinaryTree* cnt_root = cnt_layer.front();
+        TreeNode* cnt_root = cnt_layer.front();
         cnt_layer.pop();
 
-        if(data_list[i] != "null")
+        if(i < data_list.size())
         {
-            BinaryTree* left = new BinaryTree(atoi(data_list[i].c_str()));
-            cnt_root->left = left;
-            cnt_layer.push(left);
-        }
-        else
-        {
+            if(data_list[i] != "#")
+            {
+                TreeNode* left = new TreeNode(atoi(data_list[i].c_str()));
+                cnt_root->left = left;
+                cnt_layer.push(left);
+            }
             i++;
-        }
-        i++;
-        if(data_list[i] != "null")
-        {
-            BinaryTree* right = new BinaryTree(atoi(data_list[i].c_str()));
-            cnt_root->right = right;
-            cnt_left.push(right);
-        }
-        else
-        {
+            if(data_list[i] != "#")
+            {
+                TreeNode* right = new TreeNode(atoi(data_list[i].c_str()));
+                cnt_root->right = right;
+                cnt_layer.push(right);
+            }
             i++;
         }
     }
