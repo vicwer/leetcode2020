a8ac482e84cd35ad2814272e07a0f9895323a2ba
Kaige Duan <vicwer@users.noreply.github.com>
2020-05-16 23:15:09 +0800

    相同的树

--- 100.cpp
+++ 100.cpp
@@ -14,17 +14,12 @@ struct BinaryTree
 bool sameTree(BinaryTree* root1, BinaryTree* root2)
 {
     if(!root1 && !root2)
-    {
         return true;
-    }
-    else if(root1 && root2)
-    {
-        return sameTree(root1->left, root2->left) && sameTree(root1->right, root2->right) && root1->val == root2->val;
-    }
-    else
-    {
+
+    if((root1 && !root2) || (!root1 && root2))
         return false;
-    }
+
+    return sameTree(root1->left, root2->left) && sameTree(root1->right, root2->right) && root1->val == root2->val;
 }
 
 class CreateTree {
